1421a5134d2d819b2e450acfa6a5ee97c395c67d
Harrison Hascoe <hascoeharrison@gmail.com>
2020-08-28 20:45:28 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,6 +4,8 @@ def valid_move?(user)
   if user == 0,1,2,3,4,5,6,7,8
     if position_taken
     puts "The space is already filled, try again."
+  elsif ! position_taken
+    puts "Nice move"
   else 
     puts "Invaild board postion, try again"
   end
