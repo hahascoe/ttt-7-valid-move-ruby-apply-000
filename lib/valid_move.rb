54affb94adf9c66fbfb098235c89d909f375d837
Harrison Hascoe <hascoeharrison@gmail.com>
2020-08-28 20:55:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,16 +1,7 @@
 # code your #valid_move? method here
-def valid_move?(user,board)
-  puts "Enter 1-9 to pick a space on the baord!"
-  user= gets.strip 
-  if user.between'(0,8)'
-    if position_taken
-    puts "The space is already filled, try again."
-  elsif ! position_taken
-    puts "Nice move!"
-  else 
-    puts "Invaild board postion, try again"
-  end
-end 
+def valid_move?(number_entered, board)
+  number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board,index)
