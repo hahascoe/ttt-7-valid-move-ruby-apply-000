cdf6a133fecf9e23e848670f036399a12715d370
Harrison Hascoe <hascoeharrison@gmail.com>
2020-08-28 20:46:40 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(user)
   user= gets.strip 
-  if user == 0,1,2,3,4,5,6,7,8
+  if user == Integer between 0-8
     if position_taken
     puts "The space is already filled, try again."
   elsif ! position_taken
