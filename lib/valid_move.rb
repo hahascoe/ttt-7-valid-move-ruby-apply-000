5fe78a8c91867e8c306e8e76b99c93fbe59be70a
Harrison Hascoe <hascoeharrison@gmail.com>
2020-08-28 20:42:24 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,8 @@ def valid_move?
     puts "The space is already filled, try again."
   elsif user= gets.strip 
     if user == 0,1,2,3,4,5,6,7,8
-      
+  else 
+    puts "Invaild board postion"
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
