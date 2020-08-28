65cb54a6a7efa50ead9e3e0747ae7aa4f3ad5526
Harrison Hascoe <hascoeharrison@gmail.com>
2020-08-28 20:40:34 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?
   if position_taken
     puts "The space is already filled, try again."
-    
+  elsif user= gets.strip
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
