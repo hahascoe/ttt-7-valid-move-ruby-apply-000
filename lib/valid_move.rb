e559b7b825e5b2eb97c53e02fd9c5b1d647283dc
Harrison Hascoe <hascoeharrison@gmail.com>
2020-08-28 20:39:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,7 @@
 def valid_move?
   if position_taken
     puts "The space is already filled, try again."
+    
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
