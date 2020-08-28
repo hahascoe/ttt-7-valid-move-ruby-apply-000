b80778647067f2e952642f5b51ec65b2d8b50f75
Harrison Hascoe <hascoeharrison@gmail.com>
2020-08-28 21:00:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ user_input = gets.strip
 number_entered = user_input.to_i - 1
 
 def valid_move?(number_entered, board)
-  number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
+  number_entered.between?('0, 8') && !(position_taken?(board, number_entered))
 end
 
 def position_taken?(board, index)
